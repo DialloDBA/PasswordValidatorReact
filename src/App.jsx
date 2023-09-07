341ed421aef05d841e6aa13c732d4cb6b650ed6b
DialloDBA <rahamanediallo108@gmail.com>
2023-09-07 20:21:16 +0000

    Definition des variables de verification du mot de passe

--- src/App.jsx
+++ src/App.jsx
@@ -5,20 +5,29 @@ import closed from "./assets/eye-off.svg"
 
 
 function App() {
+  const [password,setPassword] = useState('')
   const [passwordIsVisible,setPasswordIsVisible] = useState(false)
+  const passwordHasValidLenght = password.length>=8;
+  const passwordHaslowerCaseLetter = /[a-z]/.test(password)
+  const passwordHasUpperCaseLetter = /[A-Z]/.test(password)
+  const passwordHasNumber = /[0-9]/.test(password)
   return (
     <>
-      <h1 className=''>Password Validation {passwordIsVisible.toString()} </h1>
-      <input type={passwordIsVisible ? 'text' : 'password'} name="password" id="" placeholder='Entrer votre mot de passe ' />
+      <h1 className=''>Password Validation {passwordHaslowerCaseLetter.toString()} </h1>
+      <input type={passwordIsVisible ? 'text' : 'password'} 
+      onChange={(e)=>setPassword(e.currentTarget.value)} 
+      name="password" id="" 
+      placeholder='Entrer votre mot de passe ' 
+      />
       <span type="button" onClick={()=>setPasswordIsVisible((prevState)=>!prevState)} >
         <img src={passwordIsVisible ? closed : open } alt={passwordIsVisible ? 'Oeil Fermer' : "Oeil Ouvert" } width={32} />
-        </span>
+      </span>
       <ul>
-        <li>Une Lettre en Minuscule</li>
-        <li>Une Lettre en Majuscule</li>
+        <li style={{color: passwordHaslowerCaseLetter ? 'green' :'red',fontWeight:passwordHaslowerCaseLetter ? 'bold' :''}}>Une Lettre en Minuscule</li>
+        <li style={{color: passwordHasUpperCaseLetter ? 'green' :'red',fontWeight:passwordHasUpperCaseLetter ? 'bold' :''}} >Une Lettre en Majuscule</li>
         <li>Un Caractère Spéciale </li>
-        <li>Un Chiffre </li>
-        <li>Minimum 8 Caractères</li>
+        <li style={{color: passwordHasNumber ? 'green' :'red',fontWeight:passwordHasNumber ? 'bold' :''}}>Un Chiffre </li>
+        <li style={{color: passwordHasValidLenght ? 'green' :'red',fontWeight:passwordHasValidLenght ? 'bold' :''}}>Minimum 8 Caractères</li>
       </ul>
     </>
   )
